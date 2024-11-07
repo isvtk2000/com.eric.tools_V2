1eec127a4615084f79b97d69105b08c158802eed
isvtk2000 <isvtk2000@gmail.com>
2024-11-07 11:50:39 +0800

    Initial commit

---
+++ Runtime/SenceControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Eric.tools
+{   /// <summary>
+    /// 場景控制
+    /// </summary>
+    public class SenceControl : MonoBehaviour
+    {
+        /// <summary>
+        /// 載入場景
+        /// </summary>
+        /// <param name="senceName"></param>
+        public void LoadSence(string senceName)
+        {
+            //載入場景
+            SceneManager.LoadScene(senceName);
+        }
+        /// <summary>
+        /// 退出遊戲
+        /// </summary>
+        public void QuitGame()
+        {
+            //離開程式
+            Application.Quit();
+        }
+    }
+}
+
+
